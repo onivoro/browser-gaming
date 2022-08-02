6fb0b5c3ebf469237730f32e0c67260a1378dfea
icedlee337 <lee.e.norris@gmail.com>
2022-08-02 12:16:55 -0400

    chore: basic animation works

---
+++ src/index.js
@@ -0,0 +1,33 @@
+class Animation {
+    constructor(imgPrefix, startIndex, endIndex) {
+        this.frameCount = endIndex;
+        this.imgPrefix = imgPrefix;
+
+    }
+
+    draw () {
+
+    }
+
+    asImage (index) {
+        const img = new Image();
+        img.src = `assets/${this.imgPrefix}/${this.imgPrefix}_${index.toString().padStart(3, '0')}.png`;
+        console.warn(img.src)
+        return img;
+    }
+}
+
+const canvas = document.getElementById('canvas');
+
+const ctx = canvas.getContext('2d');
+
+const run = new Animation('run', 0, 42);
+let i = 0;
+(function animate(time) {
+    if (i > run.frameCount) i = 0;
+    ctx.reset();
+    ctx.drawImage(run.asImage(i), 0, 0)
+    console.warn(time)
+    i++;
+    requestAnimationFrame(animate)
+})();
