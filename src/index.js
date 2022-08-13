1913d9e25b9a522d7bd563bca92d9f807a0bb116
icedlee337 <lee.e.norris@gmail.com>
2022-08-12 20:27:14 -0400

    chore: background started

--- src/index.js
+++ src/index.js
@@ -1,33 +1,33 @@
-class Animation {
-    constructor(imgPrefix, startIndex, endIndex) {
-        this.frameCount = endIndex;
-        this.imgPrefix = imgPrefix;
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const CANVAS_WIDTH = canvas.width = 800;
+const CANVAS_HEIGHT = canvas.height = 700;
+const pathToBackgrounds = 'assets/racing-background/PNG/Layers/'
+let gameSpeed = 5;
 
+const backgroundLayer1 = new Image();
+backgroundLayer1.src = pathToBackgrounds + 'layer-1.png';
+
+const backgroundLayer2 = new Image();
+backgroundLayer2.src = pathToBackgrounds + 'layer-2.png';
+
+const backgroundLayer3 = new Image();
+backgroundLayer3.src = pathToBackgrounds + 'layer-3.png';
+
+const backgroundLayer4 = new Image();
+backgroundLayer4.src = pathToBackgrounds + 'layer-4.png';
+
+const imageWidth = 2560;
+let x = 0;
+
+function animate(time) {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    ctx.drawImage(backgroundLayer3, x, 0);
+    if (x < -imageWidth) {x = 0;}
+    else { 
+        x -= gameSpeed;    
     }
-
-    draw () {
-
-    }
-
-    asImage (index) {
-        const img = new Image();
-        img.src = `assets/${this.imgPrefix}/${this.imgPrefix}_${index.toString().padStart(3, '0')}.png`;
-        console.warn(img.src)
-        return img;
-    }
+    requestAnimationFrame(animate);
 }
 
-const canvas = document.getElementById('canvas');
-
-const ctx = canvas.getContext('2d');
-
-const run = new Animation('rj', 0, 36);
-let i = 0;
-(function animate(time) {
-    if (i > run.frameCount) i = 0;
-    ctx.reset();
-    ctx.drawImage(run.asImage(i), 0, 0)
-    console.warn(time)
-    i++;
-    requestAnimationFrame(animate)
-})();
+animate(0);
